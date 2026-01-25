add4515b5d90579aaa15cbf3a4ee69332af5d5d6
Michaël COLL <michael.coll@hiit-consulting.fr>
2026-01-25 20:54:08 +0100

    chore(async) : passage a de l'async
    Closes #21

--- src/ccpt/application/repository.rs
+++ src/ccpt/application/repository.rs
@@ -1,37 +1,37 @@
-#[cfg(test)]
-use mockall::automock;
-
 use crate::domain::card::Card;
 use crate::domain::price::Price;
 use crate::domain::set_name::{SetCode, SetName};
 
+#[cfg(test)]
+use mockall::automock;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum PersistenceError {
-    SaveError(String),
+    DBError(String),
 }
 
 impl From<PersistenceError> for String {
     fn from(val: PersistenceError) -> String {
         match val {
-            PersistenceError::SaveError(msg) => msg,
+            PersistenceError::DBError(msg) => msg,
         }
     }
 }
 
 #[cfg_attr(test, automock)]
 pub trait CardRepository {
-    fn get_all(&self) -> Result<Vec<Card>, PersistenceError>;
-    fn save(&mut self, card: Card) -> Result<(), PersistenceError>;
-    fn delete_all(&mut self) -> Result<(), PersistenceError>;
+    async fn get_all(&self) -> Result<Vec<Card>, PersistenceError>;
+    async fn save(&mut self, card: Card) -> Result<(), PersistenceError>;
+    async fn delete_all(&mut self) -> Result<(), PersistenceError>;
 }
 
 #[cfg_attr(test, automock)]
 pub trait SetNameRepository {
-    fn exists_by_code(&self, code: SetCode) -> Result<bool, PersistenceError>;
-    fn save(&mut self, set: SetName) -> Result<(), PersistenceError>;
+    async fn exists_by_code(&self, code: SetCode) -> Result<bool, PersistenceError>;
+    async fn save(&mut self, set: SetName) -> Result<(), PersistenceError>;
 }
 
 #[cfg_attr(test, automock)]
 pub trait CardCollectionRepository {
-    fn save(&mut self, price: Price) -> Result<(), PersistenceError>;
+    async fn save(&mut self, price: Price) -> Result<(), PersistenceError>;
 }
