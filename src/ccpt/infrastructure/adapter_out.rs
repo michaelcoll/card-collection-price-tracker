20db173043639af6255eacf2daa8b4e73dde70c9
Michaël COLL <michael.coll@hiit-consulting.fr>
2026-01-31 13:40:05 +0100

    feat(infra) : ajout du repository des sets
    Closes #26

--- src/ccpt/infrastructure/adapter_out.rs
+++ src/ccpt/infrastructure/adapter_out.rs
@@ -1,12 +1,20 @@
 use crate::application::error::AppError;
+use crate::application::repository::PersistenceError;
 use sqlx::Error;
 
 pub mod card_entity_mapper;
 pub mod card_repository_adapter;
 pub mod entities;
+pub mod set_names_repository_adapter;
 
 impl From<Error> for AppError {
     fn from(err: Error) -> Self {
         AppError::RepositoryError(err.to_string())
+    }
+}
+
+impl From<Error> for PersistenceError {
+    fn from(_err: Error) -> Self {
+        PersistenceError::DBError(_err.to_string())
     }
 }
---
+++ src/ccpt/infrastructure/adapter_out/set_names_repository_adapter.rs
@@ -0,0 +1,112 @@
+use crate::application::error::AppError;
+use crate::application::repository::SetNameRepository;
+use crate::domain::set_name::{SetCode, SetName};
+use crate::infrastructure::adapter_out::entities::SetNameEntity;
+use sqlx::{Pool, Postgres};
+
+#[allow(dead_code)]
+pub struct SetNameRepositoryAdapter {
+    pool: Pool<Postgres>,
+}
+
+impl SetNameRepositoryAdapter {
+    #[allow(dead_code)]
+    pub fn new(pool: Pool<Postgres>) -> Self {
+        Self { pool }
+    }
+}
+
+impl SetNameRepository for SetNameRepositoryAdapter {
+    async fn exists_by_code(&self, code: SetCode) -> Result<bool, AppError> {
+        Ok(sqlx::query_as!(
+            SetNameEntity,
+            "SELECT * FROM set_name WHERE set_code = $1",
+            code.to_string()
+        )
+        .fetch_optional(&self.pool)
+        .await?
+        .is_some())
+    }
+
+    async fn save(&mut self, set: SetName) -> Result<(), AppError> {
+        sqlx::query!(
+            "INSERT INTO set_name (set_code, name)
+             VALUES ($1, $2)
+             ON CONFLICT(set_code)
+             DO UPDATE
+                SET name = $2",
+            set.code.to_string(),
+            set.name
+        )
+        .execute(&self.pool)
+        .await?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sqlx::PgPool;
+
+    #[sqlx::test]
+    async fn test_no_card_exists(pool: PgPool) {
+        let exists = SetNameRepositoryAdapter::new(pool)
+            .exists_by_code(SetCode::new("ECC"))
+            .await
+            .unwrap();
+        assert!(!exists, "no set should exist in the database");
+    }
+
+    #[sqlx::test]
+    async fn exists_by_code_returns_true_for_existing_set_code(pool: PgPool) {
+        let adapter = SetNameRepositoryAdapter::new(pool.clone());
+        let exists = adapter.exists_by_code(SetCode::new("ECL")).await.unwrap();
+        assert!(exists, "set should exist in the database");
+    }
+
+    #[sqlx::test]
+    async fn save_does_not_insert_duplicate_set_code(pool: PgPool) {
+        let mut adapter = SetNameRepositoryAdapter::new(pool.clone());
+
+        let set_name = SetName {
+            code: SetCode::new("ECL"),
+            name: "Lorwyn Eclipsed 2".to_string(),
+        };
+
+        adapter.save(set_name).await.unwrap();
+
+        let result = sqlx::query!("SELECT name FROM set_name WHERE set_code = $1", "ECL")
+            .fetch_one(&pool)
+            .await
+            .unwrap();
+
+        assert_eq!(
+            result.name, "Lorwyn Eclipsed 2",
+            "existing set name should be overridden"
+        );
+    }
+
+    #[sqlx::test]
+    async fn save_inserts_new_set_name(pool: PgPool) {
+        let mut adapter = SetNameRepositoryAdapter::new(pool.clone());
+
+        let set_name = SetName {
+            code: SetCode::new("ECC"),
+            name: "Lorwyn Eclipsed Commander".to_string(),
+        };
+
+        adapter.save(set_name).await.unwrap();
+
+        let result = sqlx::query!("SELECT name FROM set_name WHERE set_code = $1", "ECC")
+            .fetch_one(&pool)
+            .await
+            .unwrap();
+
+        assert_eq!(
+            result.name, "Lorwyn Eclipsed Commander",
+            "new set should be inserted into the database"
+        );
+    }
+}
