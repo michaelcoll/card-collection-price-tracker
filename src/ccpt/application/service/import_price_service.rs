9e6e42bae3d938c657f06e0b2492259cd21a4cf7
Michaël COLL <michael.coll@hiit-consulting.fr>
2026-06-08 21:26:59 +0200

    chore(trace+logs): configure sentry for trace and logs
    - fix of the clerk ui not loading correctly

--- src/ccpt/application/service/import_price_service.rs
+++ src/ccpt/application/service/import_price_service.rs
@@ -3,9 +3,7 @@ use crate::application::error::AppError;
 use crate::application::repository::{CardMarketPriceRepository, CardPricesViewRepository};
 use crate::application::use_case::{CardCollectionPriceCalculationUseCase, ImportPriceUseCase};
 use async_trait::async_trait;
-use colored::Colorize;
 use std::sync::Arc;
-use std::time::{SystemTime, UNIX_EPOCH};
 
 pub struct ImportPriceService {
     cardmarket_caller: Arc<dyn CardMarketCaller>,
@@ -32,25 +30,15 @@ impl ImportPriceService {
 
 #[async_trait]
 impl ImportPriceUseCase for ImportPriceService {
+    #[tracing::instrument(skip(self))]
     async fn import_prices_for_current_date(&self) -> Result<(), AppError> {
-        let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-
         let (date, price_guides) = self.cardmarket_caller.get_price_guides().await?;
         let price_count = price_guides.len();
         self.cardmarket_repository.save(date, price_guides).await?;
         self.card_prices_view_repository.refresh().await?;
         self.price_calculation.calculate_total_price().await?;
 
-        let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-        let duration = end - start;
-
-        println!(
-            "{} Imported {} prices for {} in {} ms",
-            "ℹ".yellow().bold(),
-            price_count,
-            &date,
-            duration.as_millis()
-        );
+        tracing::info!(price_count, %date, "Prices imported successfully");
 
         Ok(())
     }
