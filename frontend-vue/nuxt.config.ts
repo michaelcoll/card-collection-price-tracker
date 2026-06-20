f7fb509802eabb8546138dc44f995527547c3cd4
Michaël COLL <michael.coll@hiit-consulting.fr>
2026-06-20 16:33:37 +0200

    fix(frontend): update proxy conf

--- frontend-vue/nuxt.config.ts
+++ frontend-vue/nuxt.config.ts
@@ -65,7 +65,7 @@ export default defineNuxtConfig({
     routeRules: {
       '/api/v1/**': {
         proxy: {
-          to: 'http://localhost:8080/**',
+          to: `${process.env.NUXT_BACKEND_URL ?? 'http://localhost:8080'}/**`,
         },
       },
     },
