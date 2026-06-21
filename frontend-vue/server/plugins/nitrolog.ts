9f70d08021f68cefd1eb5e4ac104bbfaaafaea67
Michaël COLL <michael.coll@hiit-consulting.fr>
2026-06-21 11:53:58 +0200

    fix(frontend): update proxy conf
    - send nuxt server log to sentry

--- frontend-vue/server/plugins/nitrolog.ts
+++ frontend-vue/server/plugins/nitrolog.ts
@@ -1,5 +1,10 @@
+import * as Sentry from '@sentry/nuxt';
+
 export default defineNitroPlugin((nitro) => {
   nitro.hooks.hook('error', (error, { event }) => {
     console.error(`${event?.path} Application error:`, error);
+    Sentry.captureException(error, {
+      extra: { path: event?.path },
+    });
   });
 });
